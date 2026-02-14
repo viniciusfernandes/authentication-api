32428f1cb6bfddbb3eb2ab1c5284841f3e0bd7cc
vinicius <viniciussf@hotmail.com>
2026-02-14 00:09:31 -0300

    Added swagger

---
+++ src/main/java/br/com/authentication/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package br.com.authentication.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI authenticationOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Authentication API")
+                .description("Modern authentication and authorization API.")
+                .version("v1"))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
